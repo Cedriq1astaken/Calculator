b517d9b0292e974e50bcd4cd173dec28668ed037
Cedriq1astaken <113813505+Cedriq1astaken@users.noreply.github.com>
2022-11-07 03:31:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const display = document.getElementById('display');
+const deleteBtn = document.getElementById('delete')
+const circle = document.getElementById('circle')
+
+let i = 0;
+let enter = (number) =>{
+    let val = display.value.charAt(display.value.length - 1)
+    if(val == '*' || val == '/' ||val == '+' ||val == '-' ||val == '%' || val ==''){
+        if(typeof number == 'number'){
+            display.value += number
+        }
+    }
+    else{
+        display.value += number
+    }
+}
+let equal = () =>{
+    let result = eval(display.value);
+
+    (display.value == '') ? alert('Enter stuff') : display.value = result;  
+}
+let C = () =>{
+    display.value = ''
+}
+deleteBtn.onclick = () =>{
+    let str = display.value
+    str = str.slice(0, -1)
+    display.value = str
+}
+
+circle.onclick = ()=>{
+    if(i == 0){
+        circle.style.transform = 'translate(80%, -50%)';
+        i++
+        alert('Dark mode// work in progress')
+    }
+    else if(i == 1){
+        circle.style.transform = 'translate(-50%, -50%)';
+        i--
+    }
+}
